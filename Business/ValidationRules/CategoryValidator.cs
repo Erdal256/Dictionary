abdab065f273b742fd1aea4fb417136f8c3d9c9a
Erdal256 <erdall.2693@gmail.com>
2021-09-19 00:45:51 +0300

    V4

--- Business/ValidationRules/CategoryValidator.cs
+++ Business/ValidationRules/CategoryValidator.cs
@@ -12,7 +12,7 @@ namespace Business.ValidationRules
     {
         public CategoryValidator()
         {
-            RuleFor(x => x.CetegoryName).NotEmpty().WithMessage("Kategori adını boş geçemesiniz");
+            RuleFor(x => x.CetegoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz");
             RuleFor(x => x.CetegoryDescription).NotEmpty().WithMessage("Açıklamayı boş geçemezsiniz");
             RuleFor(x => x.CetegoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapın");
             RuleFor(x => x.CetegoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer giriş yapamyın");
