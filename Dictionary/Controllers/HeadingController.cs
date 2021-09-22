24321d88093b694a002e444946ca5bb3993887af
Erdal256 <erdall.2693@gmail.com>
2021-09-22 22:38:07 +0300

    V5

--- Dictionary/Controllers/HeadingController.cs
+++ Dictionary/Controllers/HeadingController.cs
@@ -46,5 +46,9 @@ namespace Dictionary.Controllers
             hm.HeadingAdd(p);
             return RedirectToAction("Index");
         }
+        public ActionResult ContentByHeading()
+        {
+            return View();
+        }
     }
 }
--- Entities/Concrete/Writer.cs
+++ Entities/Concrete/Writer.cs
@@ -15,16 +15,17 @@ namespace Entities.Concrete
         public string WriterName { get; set; }
         [StringLength(50)]
         public string WriterSurname { get; set; }
-        [StringLength(100)]
-        public string WriterImage{ get; set; }
+        [StringLength(250)]
+        public string WriterImage { get; set; }
         [StringLength(100)]
         public string WriterAbout { get; set; }
         [StringLength(200)]
         public string WriterMail { get; set; }
         [StringLength(200)]
-        public string WriterPassword{ get; set; }
+        public string WriterPassword { get; set; }
         [StringLength(50)]
         public string WriterTitle { get; set; }
+        public bool WriterStatus { get; set; }
 
         public ICollection<Heading> Headings { get; set; }
         public ICollection<Content> Contents { get; set; }
