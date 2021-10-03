8055398d4ecc3e656777ebd30a28b6bf2ee05b3e
Erdal256 <erdall.2693@gmail.com>
2021-10-03 17:02:20 +0300

    V8

--- Dictionary/Controllers/LoginController.cs
+++ Dictionary/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Dictionary.Controllers
 {
@@ -23,13 +24,15 @@ namespace Dictionary.Controllers
             var adminuserinfo = c.admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName && x.AdminPassword == p.AdminPassword);  // geriye sadece bir değer döndür - admin
             if (adminuserinfo != null)
             {
+                FormsAuthentication.SetAuthCookie(adminuserinfo.AdminUserName, false);
+                Session["AdminUserName"] = adminuserinfo.AdminUserName;
                 return RedirectToAction("Index","AdminCategory");
             }
             else
             {
                 return RedirectToAction("Index");
             }
-            return View();
+          
         }
     }
 }
---
+++ Dictionary/Controllers/WriterPanelController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dictionary.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        // GET: WriterPanel
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+        public ActionResult MyHeading()
+        {
+            //id = 4;
+            var values = hm.GetListByWriter();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CetegoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = 4;
+            p.HeadingStatus = true;
+            hm.HeadingAdd(p);
+            return RedirectToAction("MyHeading");
+        }
+    }
+}
